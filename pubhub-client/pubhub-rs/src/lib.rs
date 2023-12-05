bdd5caf196452d145d2f0c8f881c53de92e6cc03
Michał Miłek <michal.mileq@gmail.com>
2023-12-05 15:38:01 +0100

    Adapt PubHubConnection for multithreaded use of PubHub

--- pubhub-client/pubhub-rs/src/lib.rs
+++ pubhub-client/pubhub-rs/src/lib.rs
@@ -15,8 +15,15 @@ pub mod response;
 type Result<T> = std::result::Result<T, anyhow::Error>;
 
 pub struct PubHubConnection {
-    request_stream: TcpStream,
-    broadcast_listener: TcpStream,
+    request_handler: RequestHandler,
+    publish_receiver: PublishReceiver,
+}
+
+pub struct RequestHandler {
+    stream: TcpStream,
+}
+pub struct PublishReceiver {
+    stream: TcpStream,
 }
 
 trait PubHubReceiver {
@@ -35,28 +42,12 @@ impl PubHubReceiver for TcpStream {
         // Read the actual message
         let mut msg_buffer = Vec::with_capacity(size as usize);
         self.read_exact(msg_buffer.as_mut_slice())?;
-        
+
         Ok(msg_buffer)
     }
 }
 
-impl PubHubConnection {
-    pub fn new(addr: (Ipv4Addr, u16)) -> std::result::Result<Self, std::io::Error> {
-        let request_stream = TcpStream::connect(&addr)?;
-        
-        // Listen on the same port as the server + 1
-        let listener_addr = (Ipv4Addr::LOCALHOST, addr.1 + 1);
-        eprintln!("Listening on {listener_addr:?}");
-        
-        // wait for the server to reciprocate the connection
-        let (broadcast_listener, _) = TcpListener::bind(listener_addr)?.accept()?;
-
-        Ok(Self {
-            request_stream,
-            broadcast_listener,
-        })
-    }
-
+impl RequestHandler {
     pub fn execute(&mut self, request: &Request) -> Result<Response> {
         let json = request.to_json().to_string();
 
@@ -75,23 +66,25 @@ impl PubHubConnection {
         // Convert to network byte order (big endian)
         let all: &[u8] = &[size.to_be_bytes().as_slice(), msg_bytes].concat();
 
-        self.request_stream.write_all(all)?;
+        self.stream.write_all(all)?;
 
         Ok(())
     }
 
     /// Block while waiting for the next incoming response
     fn await_response(&mut self) -> Result<Response> {
-        let message_bytes = self.request_stream.next_pubhub_message()?;
+        let message_bytes = self.stream.next_pubhub_message()?;
         let msg = String::from_utf8(message_bytes)?;
         let json: Value = serde_json::from_str(&msg)?;
 
         Response::try_from(json)
     }
+}
 
+impl PublishReceiver {
     /// Receive a message from the broadcast listener
-    fn next_broadcast(&mut self) -> Result<serde_json::Value> {
-        let message_bytes = self.broadcast_listener.next_pubhub_message()?;
+    fn next_message(&mut self) -> Result<serde_json::Value> {
+        let message_bytes = self.stream.next_pubhub_message()?;
         let msg = String::from_utf8(message_bytes)?;
         let json = serde_json::from_str(&msg)?;
 
@@ -99,8 +92,36 @@ impl PubHubConnection {
     }
 }
 
-impl Drop for PubHubConnection {
-    fn drop(&mut self) {
-        let _ = self.request_stream.shutdown(std::net::Shutdown::Both);
+impl PubHubConnection {
+    pub fn new(addr: (Ipv4Addr, u16)) -> std::result::Result<Self, std::io::Error> {
+        let request_stream = TcpStream::connect(&addr)?;
+
+        // Listen on the same port as the server + 1
+        let listener_addr = (Ipv4Addr::LOCALHOST, addr.1 + 1);
+        eprintln!("Listening on {listener_addr:?}");
+
+        // wait for the server to reciprocate the connection
+        let (publish_stream, _) = TcpListener::bind(listener_addr)?.accept()?;
+
+        Ok(Self {
+            request_handler: RequestHandler {
+                stream: request_stream,
+            },
+            publish_receiver: PublishReceiver {
+                stream: publish_stream,
+            },
+        })
+    }
+
+    pub fn execute(&mut self, request: &Request) -> Result<Response> {
+        self.request_handler.execute(request)
+    }
+
+    pub fn next_message(&mut self) -> Result<serde_json::Value> {
+        self.publish_receiver.next_message()
+    }
+
+    pub fn into_inner(self) -> (RequestHandler, PublishReceiver) {
+        (self.request_handler, self.publish_receiver)
     }
 }
